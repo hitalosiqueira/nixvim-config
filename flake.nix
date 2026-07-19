13bce0a19926e37ff07fac6076efe6d494ed177d
Hitalo Siqueira <hsiqueira10@gmail.com>
2026-07-19 14:41:19 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,18 +2,14 @@
   description = "My Nixvim config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-
     nixvim = {
       url = "github:nix-community/nixvim/nixos-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = inputs: {
     nixosModules = {
-      nixvim = import ./. inputs;
+      nixvim = import ./.;
     };
-    homeManagerModules.default = import ./default.nix;
   };
 }
